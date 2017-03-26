8142232ffcc2d33f8759dece632bdd4a490069e9
Lawrence Siao <lawrence_siao@pcgbros.com>
2017-03-27 02:21:57 +0800

    fixed contend missing problem

---
+++ ptt_gossip/spiders/ptt_gossip_contend.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+import logging
+import scrapy
+from datetime import datetime
+from ptt_gossip.items import PostItem
+from scrapy.http import FormRequest
+from bs4 import BeautifulSoup
+import bs4
+import sys
+
+def loopUntilA(text, firstElement,nextATag,flag=True):
+    if flag:
+        firstElement = firstElement.next
+        
+    if type(firstElement.next) ==  bs4.element.NavigableString:
+        text += firstElement.next
+        return loopUntilA(text, firstElement.next,nextATag,flag=False)
+
+    elif type(firstElement.next) ==  bs4.element.Tag:
+        if (firstElement.next == nextATag):             
+            return text
+        else:
+        #Using double next to skip the string nodes themselves
+            return loopUntilA(text, firstElement.next,nextATag,flag=False)
+        
+
+def findNextTag(soup):
+    for tag in soup.findAll('span',{ "class" : "f2" }):
+        if dict(tag.attrs)["class"] == ['f2']:
+            return tag
+
+def getContend(soup):
+
+    firstBigTag = soup.find_all('span', {'class' : 'article-meta-value'})[-1]
+    #firstBigTag = soup.find('div', id='main-content')
+    nextATag = findNextTag(soup)
+    targetString = loopUntilA('', firstBigTag,nextATag)
+    return targetString
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class PTTSpiderContend(scrapy.Spider):
+    
+    name = 'ptt_gossip_contend'
+    allowed_domains = ['ptt.cc']
+    start_urls = ('https://www.ptt.cc/bbs/Gossiping/index.html', )
+
+    _retries = 0
+    MAX_RETRY = 10
+
+    _pages = 21326
+    MAX_PAGES = 21325
+
+    def parse(self, response):
+
+        domain = 'https://www.ptt.cc/bbs/Gossiping/'
+        if len(response.xpath('//div[@class="over18-notice"]')) > 0:
+            if self._retries < PTTSpiderContend.MAX_RETRY:
+                self._retries += 1
+                logging.warning('retry {} times...'.format(self._retries))
+                yield FormRequest.from_response(response,
+                                                formdata={'yes': 'yes'},
+                                                callback=self.parse)
+            else:
+                logging.warning('you cannot pass')
+
+        else:
+            self._pages -= 1
+            print "next pageggggg"
+            for href in response.css('.r-ent > div.title > a::attr(href)'):
+                url = response.urljoin(href.extract())
+                yield scrapy.Request(url, callback=self.parse_post)
+
+            if self._pages > self.MAX_PAGES+1:
+    
+                yield scrapy.Request(domain+ 'index' + str(self._pages) + '.html', callback=self.parse)
+
+            else:
+                logging.warning('max pages reached')
+
+    def parse_post(self, response):
+
+        res = BeautifulSoup(response.body.encode('utf-8'))
+        item = PostItem()
+        author = u'作者'
+        time = u'時間'
+        print "here"
+        item['title'] = response.xpath(
+            '//meta[@property="og:title"]/@content')[0].extract()
+
+  #      qq = res.findAll("span", { "class" : "article-meta-value" })[0].text
+        item['author'] = res.findAll("span", { "class" : "article-meta-value" })[0].text
+        datetime_str = res.findAll("span", { "class" : "article-meta-value" })[3].text
+        item['date'] = datetime.strptime(datetime_str, '%a %b %d %H:%M:%S %Y')
+
+        item['content'] = getContend(res)
+        item['url'] = response.url
+
+        yield item
